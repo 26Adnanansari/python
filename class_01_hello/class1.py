5b9cd7bae24488a0b83bc58efbcffaf7b8accd19
Adnan Ansari <138770731+26Adnanansari@users.noreply.github.com>
2024-03-02 09:32:56 +0500

    Add files via upload

---
+++ class_01_hello/class1.py
@@ -0,0 +1,6 @@
+# String 
+name = "Muhammad Qasim"         # assigning variable 
+print(name) # print
+print(type(name)) # to know type
+print(id(name)) # physcial address
+print([i for i in dir(name) if "__" not in i]) # methods and attributes
