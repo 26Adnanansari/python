32614598d9f2cef79a14a67fd48f181d44803ed8
Adnan Ansari <138770731+26Adnanansari@users.noreply.github.com>
2024-03-02 09:33:41 +0500

    Add files via upload

---
+++ class_03_operators/class3.py
@@ -0,0 +1,24 @@
+# # OPERATORS
+# * Python Operators Tutorial
+# * Operators
+# * Assignment Operators
+# * Comparison Operators
+# * Logical Operators
+# * Identity Operators
+# * Membership Operators
+# * Bitwise Operators
+
+import pandas as pd  #  this commond is advance to define at this time but for knowladge here it is using for import tables
+table = pd.read_html('https://www.w3schools.com/python/python_operators.asp')
+table[0]
+
+
+x : int = 6
+y : int = 2
+print (f"""The sum of x any y is {x+y}""")       # Addition
+print (f"""The minas of x any y is {x-y}""")     # Subtraction
+print (f"""The Multiply of x any y is {x*y}""")  # Multiplication
+print (f"""The divide of x any y is {x/y}""")    # Division
+print (f"""The Modulus of x any y is {x%y}""")   # Modulus
+print (f"""The Exponentiation of x any y is {x**y}""") # Modulus
+print (f"""The Floor division of x any y is {x//y}""") # Modulus
