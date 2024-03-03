2f472b28b3dbb29372b25c44c0155bbf8066b6e0
Adnan Ansari <138770731+26Adnanansari@users.noreply.github.com>
2024-03-03 08:25:29 +0500

    Add files via upload

---
+++ class-05_list_tuple_and_list_comprehensive/class5.py
@@ -0,0 +1,6 @@
+#    List
+#   *  Iteration operation with loop
+#   *  Apply any operation on element
+
+names : list[str] = ['Sir Zia',"Muhammad Qasim","DR Noman"] # variable name type is List and its str
+print(names)                                                # then print it
